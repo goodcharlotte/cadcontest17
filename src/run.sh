0beb56d0a9722ec3954102d0aaef2c1eecc8b522
Charlotte Lin <good-charlotte@hotmail.com>
2017-07-27 11:28:25 +0800

    auto verify

--- src/run.sh
+++ src/run.sh
@@ -5,4 +5,5 @@ fi
 ./rpgen ../testcase/unit$case_num/F.v ../testcase/unit$case_num/G.v ../testcase/unit$case_num/weight.txt patch.v out.v
 cat patch.v > final.v
 cat out.v >> final.v
-
+cp ../testcase/unit$case_num/G.v Golden.v
+./abc -f verify.script
